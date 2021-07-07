88a165ad78ff50a86319940350614a02c731ef00
Cynthia Fu <yinnycfu@gmail.com>
2021-07-07 16:05:09 +0100

    i did everything in logic

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -4,10 +4,96 @@ describe("Thermostat", function() {
   let thermostat;
 
   beforeEach(function() {
-    thermostat = new Thermostat;
+    thermostat = new Thermostat();
   });
 
   it("starts at 20 degrees", function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  it("can be increased by 1 with up()", () => {
+    thermostat.up()
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it("can be decreased by 1 with down()", () => {
+    thermostat.down()
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  it("has a minimum of 10 degrees", () => {
+    for (let i = 0; i < 11; i++) {
+      thermostat.down()
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+
+  it("has Power Saving Mode set to be on", () => {
+    expect(thermostat.isPowerSavingMode()).toBe(true);
+  });
+
+  it("can turn off Power Saving Mode", () => {
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingMode()).toBe(false);
+  });
+
+  it("can turn on Power Saving Mode", () => {
+    thermostat.switchPowerSavingModeOff();
+    thermostat.switchPowerSavingModeOn();
+    expect(thermostat.isPowerSavingMode()).toBe(true);
+  });
+
+  it("can be reset to default temp", () => {
+    thermostat.reset();
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  describe("When PSM on", () => {
+    it("has max temp of 25", () => {
+      for (let i = 0; i < 6; i++) {
+        thermostat.up()
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(25);
+    });
+  });
+
+  describe("When PSM off", () => {
+    beforeEach(function() {
+      thermostat.switchPowerSavingModeOff();
+    });
+
+    it("has max temp of 32", () => {
+      for (let i = 0; i < 13; i++) {
+        thermostat.up()
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(32);
+    });
+  });
+
+  describe("displaying usage levels", () => {
+    describe("when temp is lower than 18", () => {
+      it("has low energy usage", () => {
+        for (let i = 0; i < 3; i++) {
+          thermostat.down()
+        }
+        expect(thermostat.energyUsage()).toEqual("low-usage");
+      });
+    });
+
+    describe("when temp is between 18 and 25 inclusive", () => {
+      it("has medium energy usage", () => {
+        expect(thermostat.energyUsage()).toEqual("medium-usage");
+      });
+    });
+
+    describe("when temp is greater than 25", () => {
+      it("has high energy usage", () => {
+        for (let i = 0; i < 6; i++) {
+          thermostat.up()
+        }
+        expect(thermostat.energyUsage()).toEqual("high-usage");
+      });
+    });
+
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,6 +2,76 @@
 
 class Thermostat {
   constructor() {
-    this.temperature = 20;
+    this.MIN_TEMP = 10;
+    this.MAX_TEMP_PSM_ON = 25;
+    this.MAX_TEMP_PSM_OFF = 32;
+    this.DEFAULT_TEMP = 20;
+    this.MED_TEMP_LIMIT = 25;
+    this.LOW_TEMP_LIMIT = 18;
+    this.temperature = this.DEFAULT_TEMP;
+    this.powerSavingMode = true;
+  }
+
+  getCurrentTemperature() {
+    return this.temperature;
+  }
+
+  up() {
+    if (this._isMaxTemp()) {
+      return;
+    }
+    else {
+      this.temperature++;
+    }
+  }
+
+  down() {
+    if (this._isMinTemp()) {
+      return;
+    }
+    else {
+      this.temperature--;
+    }
+  }
+  
+  _isMinTemp() {
+    return this.temperature === this.MIN_TEMP;
+  }
+
+  _isMaxTemp() {
+    if (this.isPowerSavingMode()) {
+      return this.temperature === this.MAX_TEMP_PSM_ON;
+    }
+    else {
+      return this.temperature === this.MAX_TEMP_PSM_OFF;
+    }
+  }
+
+  isPowerSavingMode() {
+    return this.powerSavingMode;
+  }
+
+  switchPowerSavingModeOff() {
+    this.powerSavingMode = false;
+  }
+
+  switchPowerSavingModeOn() {
+    this.powerSavingMode = true;
+  }
+
+  reset() {
+    this.temperature = this.DEFAULT_TEMP;
+  }
+
+  energyUsage() {
+    if (this.getCurrentTemperature() < this.LOW_TEMP_LIMIT) {
+      return "low-usage";
+    }
+    else if (this.getCurrentTemperature() < this.MED_TEMP_LIMIT) {
+      return "medium-usage";
+    }
+    else {
+      return "high-usage";
+    }
   }
 };
