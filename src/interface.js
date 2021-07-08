ab52e352e94e14036a5a73a27b49f57bb36a88ea
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-07-08 15:45:21 +0100

    adding temperature city picker

--- src/interface.js
+++ src/interface.js
@@ -34,5 +34,27 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('#power-saving-status').innerText = 'off';
     updateTemp();
   })
-})
 
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=6865cb5acba8f25f651d919c1b524a65&units=metric')
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    document.querySelector('#current-temperature').innerText = data.main.temp;
+  });
+
+  const selectElement = document.querySelector('#current-city');
+  selectElement.addEventListener('change', (event) => {
+    const city = event.target.value;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`
+
+    fetch(url)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        document.querySelector('#current-temperature').innerText = data.main.temp;
+      })
+  });
+
+})
