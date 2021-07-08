5a3320ce90129338995bf9044798e9f0e58529a5
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-07-08 16:02:45 +0100

    adding option to input city of choice

--- src/interface.js
+++ src/interface.js
@@ -43,11 +43,9 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('#current-temperature').innerText = data.main.temp;
   });
 
-  const selectElement = document.querySelector('#current-city');
-  selectElement.addEventListener('change', (event) => {
-    const city = event.target.value;
+  const displayWeather = (city) => {
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`
-
+  
     fetch(url)
       .then((response) => {
         return response.json()
@@ -55,6 +53,15 @@ document.addEventListener("DOMContentLoaded", () => {
       .then((data) => {
         document.querySelector('#current-temperature').innerText = data.main.temp;
       })
-  });
+  }
+
+  displayWeather('London');
+
+  document.querySelector('#select-city').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const city = document.querySelector('#current-city').value;
+  
+    displayWeather(city);
+  })
 
 })
